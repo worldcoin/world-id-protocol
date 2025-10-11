aff2bcad42fda6d14eed8d30155683a4fc30641a
pd <paolodamico@users.noreply.github.com>
2025-10-11 16:09:46 -0700

    rely only on env vars

--- services/authtree-indexer/src/main.rs
+++ services/authtree-indexer/src/main.rs
@@ -1,37 +1,9 @@
 use authtree_indexer::GlobalConfig;
-use clap::Parser;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
-
-#[derive(Parser, Debug)]
-#[command(name = "authtree-indexer")]
-#[command(about = "AuthTree Indexer - tracks chain data and serves HTTP API", long_about = None)]
-struct Args {
-    /// Enable the indexer (tracks chain and writes to DB)
-    #[arg(long, default_value_t = false)]
-    indexer: bool,
-
-    /// Enable the HTTP server (serves API endpoints)
-    #[arg(long, default_value_t = false)]
-    http: bool,
-}
 
 #[tokio::main]
 async fn main() {
     let _ = dotenvy::dotenv();
-
-    let args = Args::parse();
-
-    // Determine mode based on flags
-    // If neither flag is set, default to both (backward compatibility)
-    // FIXME: Don't override on default
-    let mode = match (args.indexer, args.http) {
-        (true, true) => "both",
-        (true, false) => "indexer",
-        (false, true) => "http",
-        (false, false) => "both", // default
-    };
-
-    std::env::set_var("RUN_MODE", mode);
 
     tracing_subscriber::registry()
         .with(EnvFilter::new(
