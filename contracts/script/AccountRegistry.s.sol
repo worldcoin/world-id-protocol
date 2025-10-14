95e36335a98c80c1a511176962b88a547e6a4f91
pd <paolodamico@users.noreply.github.com>
2025-10-14 10:18:16 -0700

    add addressbook

--- contracts/script/AccountRegistry.s.sol
+++ contracts/script/AccountRegistry.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {AccountRegistry} from "../src/AccountRegistry.sol";
 
-contract CounterScript is Script {
+contract DeployScript is Script {
     AccountRegistry public accountRegistry;
 
     function setUp() public {}
