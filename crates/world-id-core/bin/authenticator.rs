2085ac92b0035f270dcd65ce15a7bf098cf8e249
pd <paolodamico@users.noreply.github.com>
2025-10-21 16:29:25 -0700

    fix imports

--- crates/world-id-core/bin/authenticator.rs
+++ crates/world-id-core/bin/authenticator.rs
@@ -1,14 +1,9 @@
 use std::{fs::File, time::Duration};
 
 use alloy::primitives::address;
-use ark_ff::UniformRand;
 use eyre::Result;
 use tokio::time::sleep;
-use world_id_core::{
-    config::Config,
-    types::{BaseField, RpRequest},
-    Authenticator, Credential,
-};
+use world_id_core::{config::Config, types::RpRequest, Authenticator, BaseField, Credential};
 
 fn install_tracing() {
     use tracing_subscriber::prelude::*;
