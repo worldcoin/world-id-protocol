125a66e36d96982a1bcf09ad69b5e4f96980e602
Fabian Gruber <fabian.gruber5@gmail.com>
2025-11-19 10:45:01 +0100

    feat: update Groth16VerifierNullifier.sol

--- contracts/test/Verifier.t.sol
+++ contracts/test/Verifier.t.sol
@@ -11,10 +11,10 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 uint256 constant credentialIssuerIdCorrect = 1;
 uint256 constant credentialIssuerIdWrong = 2;
 
-uint128 constant rpIdCorrect = 0xcb3e8192a99de46a08eeb093ef3b2144;
+uint128 constant rpIdCorrect = 0xa3d3be8a7b705148db53d2bb75cf436b;
 uint128 constant rpIdWrong = 2;
 
-uint256 constant rootCorrect = 0x11e122f688a8b3e47f02c77e0ccc7fa083ac6578be56690d72b300f2c3982846;
+uint256 constant rootCorrect = 0x1d22549d78774db0d351d984a476f26fca780643e134f435ad966c29c4652122;
 uint256 constant rootWrong = 2;
 
 contract RpRegistryMock {
@@ -23,8 +23,8 @@ contract RpRegistryMock {
     function getRpNullifierKey(uint128 rpId) external view returns (Types.BabyJubJubElement memory) {
         if (rpId == rpIdCorrect) {
             return Types.BabyJubJubElement({
-                x: 0x091b6c36599c463334b4921b3dcd0aa2170f3081007b33364ca033ea1442ccc0,
-                y: 0x11f62caebb20e2db44861e4ae188bc954243591c5bb677f84e1560fdb1cb1de6
+                x: 0x158bde45465f643c741ec671211d8cdda47f2015843d5d8d6f0fd3823773b08e,
+                y: 0x6cd134f217937f3f88d19f9418a67d481b67c87ce959e51f08d18ea76972d8b
             });
         } else {
             return Types.BabyJubJubElement({
@@ -50,8 +50,8 @@ contract CredentialSchemaIssuerRegistryMock {
     {
         if (issuerSchemaId == credentialIssuerIdCorrect) {
             return CredentialSchemaIssuerRegistry.Pubkey({
-                x: 0x11472d905dbd235545a10fe003712f02ccb337feae307d9e2cc98572a9e49949,
-                y: 0x1b2d6ece3d71105ab4658320564b9a8ea4fedb8bca5e1b01986aaefe87c43cd7
+                x: 0x29a0a9a447716d534de135000087753a335dba7af26fafb88209d837cacf5631,
+                y: 0x2b400d3ada4bafcb92128545f2498fe10db3932df5f65dc029d11246f3602e38
             });
         } else {
             return CredentialSchemaIssuerRegistry.Pubkey({
@@ -68,34 +68,36 @@ contract NullifierVerifier is Test {
 
     address public verifierGroth16;
 
-    uint256 accountCommitment = 0x2227c04ecb75e771b827685ab5622a67a08d557f00b2ab6867d03afd11b06624;
-    uint256 nullifier = 0x037fddd3d405b27286ffe9b472bf9b10a5df1e6ae06a6e6014392dfc51a0e3da;
-    uint256 proofTimestamp = 0x00000000000000000000000000000000000000000000000000000000690e0683;
-    uint128 rpId = 0xcb3e8192a99de46a08eeb093ef3b2144;
-    uint256 action = 0x1cd71ef9ab434e89a42c11587ea8f554331f10001fb292ba8cd798dd62385e15;
-    uint256 rpKey_x = 0x091b6c36599c463334b4921b3dcd0aa2170f3081007b33364ca033ea1442ccc0;
-    uint256 rpKey_y = 0x11f62caebb20e2db44861e4ae188bc954243591c5bb677f84e1560fdb1cb1de6;
-    uint256 signalHash = 0x21677a0062b3c7a73f76603fc57af2006dc41b4c0bf952e77e92491f91d74ce9;
-    uint256 nonce = 0x03003f4a26ec49b51cbd7b3e15a880770e0a90f189757f46524d5619e8408a11f;
+    uint256 public proofTimestampDelta;
+
+    uint256 accountCommitment = 0x08987cf30dc2d612c1ff5b578e13c88e79c93f97ce5b5de38cd32398e38b49e0;
+    uint256 nullifier = 0x5a691b2dce9717b041201d1050b716c2c53626b71283c4dfa8a69a1f05e0500;
+    uint256 proofTimestamp = 0x691c5060;
+    uint128 rpId = 0xa3d3be8a7b705148db53d2bb75cf436b;
+    uint256 action = 0x5af36be93f35ed0611d38e6f759aade2532563da3bf91fbf251bedb228c4326;
+    uint256 rpKey_x = 0x158bde45465f643c741ec671211d8cdda47f2015843d5d8d6f0fd3823773b08e;
+    uint256 rpKey_y = 0x6cd134f217937f3f88d19f9418a67d481b67c87ce959e51f08d18ea76972d8b;
+    uint256 signalHash = 0x2ecfa99ecb77772534c42713e20a21ff36c838870a6a3846fd1c4667326ca5e5;
+    uint256 nonce = 0x2005e5e4b247df0f284a7e717835b18d18dfddcbb8f65c31fa22edbb047d78ea;
 
     Types.Groth16Proof proof = Types.Groth16Proof({
         a: [
-            0x266fe1cb3647102c92f073f3818641aca99ba997a6101fd90b5d2b231ec6fd67,
-            0x2f5d33a9de94fd5045377234205a25aac7ffddabae3b1c4e6557e49639c8542d
+            0x187f24c372a1c42c8a8ed9c74592210ac3fa4337d810c401dbd313a2e9424f03,
+            0x13fff489f24d745ecb90697e33d35e02e1c1f14c2ee28d4677c8fdf5d19ba947
         ],
         b: [
             [
-                0x2aa798b9e477fe79018edebce512c0a129ca20bead4337a3fe686d7d7edcbc95,
-                0x0c8ddd2b5540d567cf57a1a56278b3b6ec3efbc6f8cdcc4a0c3923e718a40925
+                0x2586480928ac0651b735c8024d575d2f5d59c2946c305e123a867ae88fd83600,
+                0x07f3795a9842c1a41ca17f0f7c275efa0da09f214d61267126f7f317afc39133
             ],
             [
-                0x158ae9ea9aa816b3ecad7b6e9af27b677ede447128c44239f46547ad50647317,
-                0x1a66279399270c4231efadea2c1ccd7a4bfb9b6c77e1a5fba0bc156500bc14d9
+                0x00d20fbd777d3648b1670dcf46f12ad52dbb56aa4bb58a3cdf0b5c97d426e1f1,
+                0x1e8306e907e700e1f9160eee3bc9cc7fe988bb66a28f232e43d988c32675b50a
             ]
         ],
         c: [
-            0x2750f5c10227b80d42d016df30e3ada05d1ac995a99de5491cdd040b346e68b3,
-            0x0a0552a79c0d862111e78e1cdc090fb52d7805e804f65ed6ff35993eca58e8f1
+            0x220825adf76ca3730ce0241de7cf5ce388d4d275886fe7b01d82155c55915d24,
+            0x202410723daf76348a7d4123e867676e27375a95e70a06815b8895f9d21f8ecb
         ]
     });
 
@@ -169,22 +171,22 @@ contract NullifierVerifier is Test {
     function test_WrongProof() public {
         Types.Groth16Proof memory brokenProof = Types.Groth16Proof({
             a: [
-                0x266fe1cb3647102c92f073f3818641aca99ba997a6101fd90b5d2b231ec6fd67,
-                0x2f5d33a9de94fd5045377234205a25aac7ffddabae3b1c4e6557e49639c8542d
+                0x220825adf76ca3730ce0241de7cf5ce388d4d275886fe7b01d82155c55915d24,
+                0x202410723daf76348a7d4123e867676e27375a95e70a06815b8895f9d21f8ecb
             ],
             b: [
                 [
-                    0x2aa798b9e477fe79018edebce512c0a129ca20bead4337a3fe686d7d7edcbc95,
-                    0x0c8ddd2b5540d567cf57a1a56278b3b6ec3efbc6f8cdcc4a0c3923e718a40925
+                    0x2586480928ac0651b735c8024d575d2f5d59c2946c305e123a867ae88fd83600,
+                    0x07f3795a9842c1a41ca17f0f7c275efa0da09f214d61267126f7f317afc39133
                 ],
                 [
-                    0x158ae9ea9aa816b3ecad7b6e9af27b677ede447128c44239f46547ad50647317,
-                    0x1a66279399270c4231efadea2c1ccd7a4bfb9b6c77e1a5fba0bc156500bc14d9
+                    0x00d20fbd777d3648b1670dcf46f12ad52dbb56aa4bb58a3cdf0b5c97d426e1f1,
+                    0x1e8306e907e700e1f9160eee3bc9cc7fe988bb66a28f232e43d988c32675b50a
                 ]
             ],
             c: [
-                0x2750f5c10227b80d42d016df30e3ada05d1ac995a99de5491cdd040b346e68b3,
-                0x0a0552a79c0d862111e78e1cdc090fb52d7805e804f65ed6ff35993eca58e8f
+                0x187f24c372a1c42c8a8ed9c74592210ac3fa4337d810c401dbd313a2e9424f03,
+                0x13fff489f24d745ecb90697e33d35e02e1c1f14c2ee28d4677c8fdf5d19ba947
             ]
         });
         vm.warp(proofTimestamp + 1 hours);
