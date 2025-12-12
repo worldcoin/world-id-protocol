e0d4f845ed1bd2b5988921eaa9d40c1b12aa8915
Fabian Gruber <fabian.gruber5@gmail.com>
2025-12-12 08:55:46 +0100

    chore: only dummy compile deps of OprfKeyRegistry.sol

--- contracts/src/OprfKeyRegistryDummy.sol
+++ contracts/src/OprfKeyRegistryDummy.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {OprfKeyRegistry} from "oprf-service/src/OprfKeyRegistry.sol";
 import {Verifier as VerifierKeyGen13} from "oprf-service/src/VerifierKeyGen13.sol";
 import {BabyJubJub} from "oprf-service/src/BabyJubJub.sol";
 
-// This file exists just to force Forge to compile LibToken.sol from some-dependency.
+// This file exists just to force Forge to compile dependencies of OprfKeyRegistry.sol
 contract __Foundry_Compile_OprfKeyRegistry_Dummy {}
