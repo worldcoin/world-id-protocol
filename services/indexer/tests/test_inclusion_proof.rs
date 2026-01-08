79f6f120199278c9243c7264acd43dd7e96192e7
Dawid Cichuta <cichaczem@gmail.com>
2026-01-08 22:49:12 +0100

    Add basic integration tests

--- services/indexer/tests/test_inclusion_proof.rs
+++ services/indexer/tests/test_inclusion_proof.rs
@@ -9,7 +9,7 @@ use http::StatusCode;
 use serial_test::serial;
 use sqlx::types::Json;
 use world_id_core::EdDSAPrivateKey;
-use world_id_indexer::config::{Environment, GlobalConfig, HttpConfig, IndexerConfig, RunMode};
+use world_id_indexer::config::{Environment, GlobalConfig, HttpConfig, IndexerConfig, RunMode, TreeCacheConfig};
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 #[serial]
@@ -34,6 +34,7 @@ async fn test_backfill_and_live_sync() {
         )
         .await;
 
+    let temp_cache_path = std::env::temp_dir().join(format!("test_cache_{}.mmap", uuid::Uuid::new_v4()));
     let global_config = GlobalConfig {
         environment: Environment::Development,
         run_mode: RunMode::Both {
@@ -51,6 +52,11 @@ async fn test_backfill_and_live_sync() {
         db_url: setup.db_url.clone(),
         rpc_url: setup.rpc_url(),
         registry_address: setup.registry_address,
+        tree_cache: TreeCacheConfig {
+            cache_file_path: temp_cache_path.to_str().unwrap().to_string(),
+            dense_tree_prefix_depth: 20,
+            http_cache_refresh_interval_secs: 30,
+        },
     };
 
     let indexer_task = tokio::spawn(async move {
@@ -135,6 +141,7 @@ async fn test_backfill_and_live_sync() {
 async fn test_insertion_cycle_and_avoids_race_condition() {
     let setup = TestSetup::new().await;
 
+    let temp_cache_path = std::env::temp_dir().join(format!("test_cache_{}.mmap", uuid::Uuid::new_v4()));
     let global_config = GlobalConfig {
         environment: Environment::Development,
         run_mode: RunMode::HttpOnly {
@@ -147,6 +154,11 @@ async fn test_insertion_cycle_and_avoids_race_condition() {
         db_url: setup.db_url.clone(),
         rpc_url: setup.rpc_url(),
         registry_address: setup.registry_address,
+        tree_cache: TreeCacheConfig {
+            cache_file_path: temp_cache_path.to_str().unwrap().to_string(),
+            dense_tree_prefix_depth: 20,
+            http_cache_refresh_interval_secs: 30,
+        },
     };
 
     let http_task = tokio::spawn(async move {
