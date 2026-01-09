2764aec2b1560120c9844f4e30c195f4216a6096
Fabian <49373262+fabian1409@users.noreply.github.com>
2026-01-09 23:28:34 +0100

    chore: don't publish world-id-oprf-node and world-id-oprf-dev-client (#168)

--- contracts/src/OprfKeyRegistryDummy.sol
+++ contracts/src/OprfKeyRegistryDummy.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Verifier as VerifierKeyGen13} from "oprf-service/src/VerifierKeyGen13.sol";
-import {BabyJubJub} from "oprf-service/src/BabyJubJub.sol";
+import {Verifier as VerifierKeyGen13} from "oprf-key-registry/src/VerifierKeyGen13.sol";
+import {BabyJubJub} from "oprf-key-registry/src/BabyJubJub.sol";
 
 // This file exists just to force Forge to compile dependencies of OprfKeyRegistry.sol
 contract __Foundry_Compile_OprfKeyRegistry_Dummy {}
