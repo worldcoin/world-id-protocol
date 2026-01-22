388e31717ec37630d247bac9d11f4a58139f0248
Karan Kurbur <karankurbur@gmail.com>
2026-01-22 15:35:26 -0800

    use solidity ^0.8.13 in all files

--- contracts/test/VerifierNullifier.t.sol
+++ contracts/test/VerifierNullifier.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {Verifier} from "../src/VerifierNullifier.sol";
