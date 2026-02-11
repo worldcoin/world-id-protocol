819d59d6b990cecd6bb033ab4eff6c68c125150e
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-02-11 12:31:27 +0100

    refactor: oprf-node main wraped into run function for tracing clarity (#396)
    * chore: log when watchers closed stream
    
    * refactor: oprf-node main wraped into run function for tracing clarity
    
    * build(deps): updated taceo images
    
    * chore: update oprf-key-registry forge dep + oprf-key-gen docker images
    
    ---------
    
    Co-authored-by: Fabian Gruber <fabian.gruber5@gmail.com>

--- services/oprf-node/src/auth/schema_issuer_registry_watcher.rs
+++ services/oprf-node/src/auth/schema_issuer_registry_watcher.rs
@@ -98,7 +98,10 @@ impl SchemaIssuerRegistryWatcher {
                 loop {
                     let log = tokio::select! {
                         log = stream.next() => {
-                            log.ok_or_else(||eyre::eyre!("SchemaIssuerRegistryWatcher subscribe stream was closed"))?
+                            log.ok_or_else(||{
+                                tracing::warn!("SchemaIssuerRegistryWatcher subscribe stream was closed");
+                                eyre::eyre!("SchemaIssuerRegistryWatcher subscribe stream was closed")
+                            })?
                         }
                         _ = cancellation_token.cancelled() => {
                             break;
