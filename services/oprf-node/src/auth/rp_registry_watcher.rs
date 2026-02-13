228401a54d8e52e4a51371665bc6f386e7b14d7c
alessandromazza98 <alessandro98.mazza@gmail.com>
2026-02-13 09:45:52 +0100

    fix: don't update oprf key id in RpRegistryWatcher

--- services/oprf-node/src/auth/rp_registry_watcher.rs
+++ services/oprf-node/src/auth/rp_registry_watcher.rs
@@ -32,10 +32,7 @@ alloy::sol! {
     #[allow(missing_docs, clippy::too_many_arguments)]
     #[sol(rpc)]
     RpRegistry,
-    concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/../../contracts/out/RpRegistry.sol/RpRegistry.json"
-    )
+    "abi/RpRegistryAbi.json"
 }
 
 #[derive(Clone, Debug)]
@@ -58,7 +55,10 @@ pub(crate) enum RpRegistryWatcherError {
 
 /// Monitors the RPs from the RpRegistry contract.
 ///
-/// RPs are lazily loaded, meaning in the beginning the store will be empty. When valid requests are coming in from users, this service will go to chain and try fetching the ecdsa keys and store them up to a configurable maximum.
+/// RPs are lazily loaded, meaning in the beginning the store will be empty.
+/// 
+/// When valid requests are coming in from users, this service will go to chain
+/// and try fetching the ecdsa keys and store them up to a configurable maximum.
 ///
 /// Additionally, will subscribe to chain events to handle RpUpdate events.
 #[derive(Clone)]
@@ -126,8 +126,8 @@ impl RpRegistryWatcher {
                                         if let Some(entry) = entry {
                                             tracing::debug!("updating rp {rp_id} in store");
                                             let mut rp = entry.into_value();
+                                            // Note: we don't update oprf key id because it cannot mutate
                                             rp.signer = event.signer;
-                                            rp.oprf_key_id = OprfKeyId::new(event.oprfKeyId);
                                             Op::Put(rp)
                                         } else {
                                             tracing::debug!(
