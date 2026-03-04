a659251f1fcb48013cf4b642f96ed76672e52bab
0xOsiris <djosiris@proton.me>
2026-03-04 10:47:27 -0800

    chore: cleanup bin

--- services/relay/src/bin/main.rs
+++ services/relay/src/bin/main.rs
@@ -5,8 +5,8 @@ use world_id_relay::cli::Cli;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let env_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join(".env");
-    let _ = dotenvy::from_path(&env_path);
+    dotenvy::dotenv().ok();
+
     let _guard = telemetry_batteries::init();
 
     tracing::info!("starting world-id-relay");
