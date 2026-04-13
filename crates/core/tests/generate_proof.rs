6041a549d9965d1b6d5a4bb297f4ab134527745b
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-04-13 07:45:42 +0200

    build(oprf-node): bump taceo crates

--- crates/core/tests/generate_proof.rs
+++ crates/core/tests/generate_proof.rs
@@ -11,8 +11,10 @@ use alloy::{
     signers::local::LocalSigner,
 };
 use eyre::{Context as _, Result, eyre};
-use taceo_oprf::types::{OprfKeyId, ShareEpoch};
-use taceo_oprf_test_utils::health_checks;
+use taceo_oprf::{
+    dev_client::health_checks,
+    types::{OprfKeyId, ShareEpoch},
+};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
 use world_id_core::{
@@ -174,8 +176,10 @@ async fn e2e_authenticator_generate_proof() -> Result<()> {
 
     let rp_fixture = generate_rp_fixture();
 
+    let (_postgres, connection_string) = taceo_oprf_test_utils::postgres_testcontainer().await?;
+
     let (key_gen_secret_managers, node_secret_managers) =
-        world_id_test_utils::stubs::init_test_secret_managers();
+        world_id_test_utils::stubs::init_test_secret_managers(connection_string.into()).await?;
 
     // OPRF key-gen instances
     let oprf_key_gens = world_id_test_utils::stubs::spawn_key_gens(
