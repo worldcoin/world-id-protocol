afb3558c209cb3f71f4143155af3e750f03faf9d
Paolo D'Amico <paolodamico@users.noreply.github.com>
2026-04-17 06:01:45 +0200

    chore: fix registries crate (#678)

--- crates/registry/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Contract bindings and signing utilities for World ID on-chain registries.
-
-#![cfg_attr(not(test), deny(unused_crate_dependencies))]
-#![deny(clippy::all, clippy::nursery, missing_docs)]
-
-pub mod world_id;
