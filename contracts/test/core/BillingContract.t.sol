d3f7207c73ea5665a3241a94e5c0ec7087b1334d
kilianglas <glas_k@yahoo.de>
2026-07-07 13:07:34 +0200

    style: format per ci forge fmt

--- contracts/test/core/BillingContract.t.sol
+++ contracts/test/core/BillingContract.t.sol
@@ -181,10 +181,7 @@ contract BillingContractTest is Test {
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", billing.DOMAIN_SEPARATOR(), structHash));
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, digest);
         return IBillingContract.SignedVoteChunk({
-            chunkIndex: chunkIndex,
-            isFinal: isFinal,
-            counts: counts,
-            signature: abi.encodePacked(r, s, v)
+            chunkIndex: chunkIndex, isFinal: isFinal, counts: counts, signature: abi.encodePacked(r, s, v)
         });
     }
 
