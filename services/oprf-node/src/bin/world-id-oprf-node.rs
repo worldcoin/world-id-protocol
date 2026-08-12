600d2a8da0e86e9d2a558aefe0ebe68217ab2a2e
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-08-12 10:38:39 +0200

    config over serde_env again

--- services/oprf-node/src/bin/world-id-oprf-node.rs
+++ services/oprf-node/src/bin/world-id-oprf-node.rs
@@ -4,7 +4,6 @@
 //! It initializes tracing, metrics, and starts the node with configuration
 //! from command-line arguments or environment variables.
 
-use taceo_nodes_common::postgres::PostgresConfig;
 #[cfg(not(target_env = "msvc"))]
 use tikv_jemallocator::Jemalloc;
 
@@ -14,8 +13,10 @@ static GLOBAL: Jemalloc = Jemalloc;
 
 use std::{net::SocketAddr, process::ExitCode, sync::Arc};
 
+use config::{Config, Environment};
 use eyre::Context;
 use serde::Deserialize;
+use taceo_nodes_common::postgres::PostgresConfig;
 use taceo_oprf::service::secret_manager::{SecretManager, postgres::PostgresSecretManager};
 use world_id_oprf_node::config::WorldOprfNodeConfig;
 
@@ -34,8 +35,16 @@ struct FullWorldOprfNodeConfig {
 
 // we are not allowed to build an eyre::Report yet because telemetry-batteries expects to install
 // the color-eyre hook
-fn load_world_id_config() -> Result<FullWorldOprfNodeConfig, serde_env::Error> {
-    let oprf_config = serde_env::from_env_with_prefix("TACEO_OPRF_NODE");
+fn load_world_id_config() -> Result<FullWorldOprfNodeConfig, config::ConfigError> {
+    let cfg = Config::builder().add_source(
+        Environment::with_prefix("TACEO_OPRF_NODE")
+            .separator("__")
+            .list_separator(",")
+            .with_list_parse_key("service.rpc.http_urls")
+            .try_parsing(true),
+    );
+
+    let oprf_config = cfg.build()?.try_deserialize()?;
 
     // Unset all env vars with our prefix to prevent leakage to subprocesses.
     // Safety: this is called before any threads are spawned.
@@ -49,7 +58,7 @@ fn load_world_id_config() -> Result<FullWorldOprfNodeConfig, serde_env::Error> {
         }
     }
 
-    oprf_config
+    Ok(oprf_config)
 }
 
 fn default_bind_addr() -> SocketAddr {
