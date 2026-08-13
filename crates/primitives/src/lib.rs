f4c179808c6273a4a5465defeb717679aa275adb
pd <paolodamico@users.noreply.github.com>
2026-08-13 09:46:10 -0700

    fix!: don't expose fe with modulo reduction

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -133,9 +133,10 @@ impl FieldElement {
     /// # Warning
     /// Use this function carefully. This function will perform modulo reduction on the input, which may
     /// lead to unexpected results if the input should not be reduced. For example, this is **not** appropriate
-    /// when parsing a canonical field-element encoding.
+    /// when parsing a canonical field-element encoding. Because of the potential footgun, it is not exposed beyond
+    /// this crate.
     #[must_use]
-    pub fn from_be_bytes_mod_order(bytes: &[u8]) -> Self {
+    pub(crate) fn from_be_bytes_mod_order(bytes: &[u8]) -> Self {
         let field_element = Fq::from_be_bytes_mod_order(bytes);
         Self(field_element)
     }
